baf9d6248b857052bac08a1af3791c708d67012f
jasonf8080 <87998220+jasonf8080@users.noreply.github.com>
2021-10-13 11:39:03 -0400

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,481 @@
+const menu = [
+    {
+        category: 'appetizer',
+        image: 'https://www.dinneratthezoo.com/wp-content/uploads/2019/12/mozzarella-sticks-4-500x500.jpg',
+        title:  'Mozzerella Sticks',
+        price:  '$9.99',
+        desc:   'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.',
+    },
+
+    {
+        category: 'appetizer',
+        image: 'https://images.themodernproper.com/billowy-turkey/production/posts/2019/garlic-bread-10.jpg?w=1200&h=1200&q=82&fm=jpg&fit=crop&fp-x=0.5&fp-y=0.5&dm=1599767628&s=08fa1a2e6dd3daeba5175c6eb9a2d06f',
+        title:  'Garlic Bread',
+        price:  '$9.99',
+        desc:   'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.',
+    },
+
+    {
+        category: 'appetizer',
+        image: 'https://dinnerthendessert.com/wp-content/uploads/2018/08/Buffalo-Wings-4-500x500.jpg',
+        title:  'Buffalo Wings',
+        price:  '$9.99',
+        desc:   'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.',
+    },
+
+    {
+        category: 'appetizer',
+        image: 'https://easyweeknightrecipes.com/wp-content/uploads/2020/07/Bruschetta-9-500x500.jpg',
+        title:  'Fresh Bruschetta',
+        price:  '$9.99',
+        desc:   'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.',
+    },
+
+    {
+        category: 'appetizer',
+        image: 'https://www.spendwithpennies.com/wp-content/uploads/2019/06/Garlic-Grilled-Shrimp-SpendWithPennies-3-500x500.jpg',
+        title:  'Grilled Shrimp',
+        price:  '$9.99',
+        desc:   'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.',
+    },
+
+    {
+        category: 'appetizer',
+        image: 'https://paleogrubs.com/wp-content/uploads/2014/12/parsnipfriesrecipe.jpg',
+        title:  'Truffle Fries',
+        price:  '$9.99',
+        desc:   'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.',
+    },
+
+    {
+        category: 'appetizer',
+        image: 'https://www.dinneratthezoo.com/wp-content/uploads/2019/11/fried-calamari-67-500x500.jpg',
+        title:  'Fried Calamari',
+        price:  '$9.99',
+        desc:   'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.',
+    },
+
+    {
+        category: 'appetizer',
+        image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/stuffed-mushrooms-vertical-jpg-1525207616.jpg?crop=1.00xw:0.667xh;0,0.215xh&resize=480:*',
+        title:  'Stuffed Mushrooms',
+        price:  '$9.99',
+        desc:   'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.',
+    },
+
+    {
+        category: 'salads',
+        image: 'https://www.jessicagavin.com/wp-content/uploads/2019/07/caesar-salad-10-1200.jpg',
+        title:  'Caesar Salad',
+        price:  '$9.99',
+        desc:   'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.',
+    },
+
+    {
+        category: 'salads',
+        image: 'https://images.themodernproper.com/billowy-turkey/production/posts/2019/Easy-italian-salad-recipe-10.jpg?w=1200&h=1200&q=82&fm=jpg&fit=crop&fp-x=0.5&fp-y=0.5&dm=1614096227&s=c0f63a30cef3334d97f9ecad14be51da',
+        title:  'Italian Salad',
+        price:  '$9.99',
+        desc:   'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.',
+    },
+
+    {
+        category: 'salads',
+        image: 'https://cravingtasty.com/wp-content/uploads/2019/02/Grilled-Chicken-Salad-Horizontal-500x500.jpg',
+        title:  'Grilled Chicken Salad',
+        price:  '$9.99',
+        desc:   'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.',
+    },
+
+
+    {
+        category: 'salads',
+        image: 'https://www.spoonfulofflavor.com/wp-content/uploads/2020/03/garden-salad-recipe-500x500.jpg',
+        title:  'Fresh Garden Salad',
+        price:  '$9.99',
+        desc:   'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.',
+    },
+
+    {
+        category: 'salads',
+        image: 'https://www.daringgourmet.com/wp-content/uploads/2020/10/Minestrone-12.jpg',
+        title:  'Minestrone Soup',
+        price:  '$9.99',
+        desc:   'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.',
+    },
+
+    {
+        category: 'salads',
+        image: 'https://www.willcookforsmiles.com/wp-content/uploads/2020/01/pasta-e-fagioli-8-1-500x500.jpg',
+        title:  'Pasta Fagoli',
+        price:  '$9.99',
+        desc:   'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.',
+    },
+
+
+    {
+        category: 'salads',
+        image: 'https://assets.bonappetit.com/photos/5c634f0d050c433e0a336d6a/1:1/w_2560%2Cc_limit/healthyish-french-onion-horizontal.jpg',
+        title:  'French Onion Soup',
+        price:  '$9.99',
+        desc:   'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.',
+    },
+    
+
+    {
+        category: 'salads',
+        image: 'https://static.fanpage.it/wp-content/uploads/sites/22/2020/11/Creamy-Potato-Soup-13-1200x1200.jpg',
+        title:  'Creamy Potato Soup',
+        price:  '$9.99',
+        desc:   'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.',
+    },
+
+    {
+        category: 'entrees',
+        image: 'https://images.themodernproper.com/billowy-turkey/production/posts/2021/Chicken-Parmesan-14.jpg?w=1200&h=1200&q=82&fm=jpg&fit=crop&fp-x=0.5&fp-y=0.5&dm=1618755606&s=cb9a4e64a5cd5948c2156f53ca5312ed',
+        title:  'Chicken Parmagiana',
+        price:  '$9.99',
+        desc:   'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.',
+    },
+
+    {
+        category: 'entrees',
+        image: 'https://www.yellowblissroad.com/wp-content/uploads/2019/07/Shrimp-Scampi-SQUARE.jpg',
+        title:  'Shrimp Scampi',
+        price:  '$9.99',
+        desc:   'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.',
+    },
+
+    {
+        category: 'entrees',
+        image: 'https://keytomylime.com/wp-content/uploads/2021/07/Best-Creamy-Cajun-Chicken-Pasta-Recipe-735x735.jpg',
+        title:  'Cajun Chicken Pasta',
+        price:  '$9.99',
+        desc:   'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.',
+    },
+
+    {
+        category: 'entrees',
+        image: 'https://www.jocooks.com/wp-content/uploads/2020/07/skillet-steak-1-7-500x500.jpg',
+        title:  'Wood Fired Steak',
+        price:  '$9.99',
+        desc:   'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.',
+    },
+
+    {
+        category: 'entrees',
+        image: 'https://www.thechunkychef.com/wp-content/uploads/2017/07/Bourbon-Bacon-Cheeseburgers-sideview-680x738.jpg',
+        title:  'Classic Burger',
+        price:  '$9.99',
+        desc:   'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.',
+    },
+
+    {
+        category: 'entrees',
+        image: 'https://cafedelites.com/wp-content/uploads/2017/03/Best-Crispy-Lemon-Garlic-Herb-Salmon-IMAGES-19-500x500.jpg',
+        title:  'Salmon Filet',
+        price:  '$9.99',
+        desc:   'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.',
+    },
+
+    {
+        category: 'entrees',
+        image: 'https://static.onecms.io/wp-content/uploads/sites/44/2019/08/26231103/5565807.jpg',
+        title:  'Maryland Style Crab Cakes',
+        price:  '$9.99',
+        desc:   'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.',
+    },
+
+    {
+        category: 'entrees',
+        image: 'https://neighborfoodblog.com/wp-content/uploads/2019/09/stuffed-pork-tenderloin-4-720x720.jpg',
+        title:  'Pork Tenderloin',
+        price:  '$9.99',
+        desc:   'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.',
+    },
+
+    {
+        category: 'desserts',
+        image: 'https://www.theflavorbender.com/wp-content/uploads/2019/01/Molten-Lava-Cake-The-Flavor-Bender-Featured2.jpg',
+        title:  'Chocolate Lava Cake',
+        price:  '$9.99',
+        desc:   'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.',
+    },
+
+    {
+        category: 'desserts',
+        image: 'https://www.livewellbakeoften.com/wp-content/uploads/2021/01/Tiramisu-8s.jpg',
+        title:  'Tiramisu',
+        price:  '$9.99',
+        desc:   'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.',
+    },
+
+    {
+        category: 'desserts',
+        image: 'https://www.recipetineats.com/wp-content/uploads/2018/09/Chocolate-Mousse_9.jpg?w=500&h=500&crop=1',
+        title:  'Chocolate Mousse',
+        price:  '$9.99',
+        desc:   'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.',
+    },
+
+    {
+        category: 'desserts',
+        image: 'https://handletheheat.com/wp-content/uploads/2020/10/BAKERY-STYLE-CHOCOLATE-CHIP-COOKIES-9-637x637-1.jpg',
+        title:  'Chewy Chocolate Chip Cookies',
+        price:  '$9.99',
+        desc:   'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.',
+    },
+
+    {
+        category: 'desserts',
+        image: 'https://www.alattefood.com/wp-content/uploads/2015/12/Carrot-Spice-Cake-Recipe-10-500x500.jpg',
+        title:  'Carrot Cake',
+        price:  '$9.99',
+        desc:   'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.',
+    },
+
+    {
+        category: 'desserts',
+        image: 'https://handletheheat.com/wp-content/uploads/2014/09/How-to-Make-Eclairs-SQUARE.jpg',
+        title:  'Eclairs',
+        price:  '$9.99',
+        desc:   'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.',
+    },
+
+    {
+        category: 'desserts',
+        image: 'https://amandascookin.com/wp-content/uploads/2021/07/Cream-Puffs-SQ.jpg',
+        title:  'Cream Puffs',
+        price:  '$9.99',
+        desc:   'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.',
+    },
+
+    {
+        category: 'desserts',
+        image: 'https://lovebakesgoodcakes.com/wp-content/uploads/2020/07/Almond-Bear-Claw-square.jpg',
+        title:  'Almond Bear Claw',
+        price:  '$9.99',
+        desc:   'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.',
+    }
+
+]
+
+const options = document.querySelectorAll('.option');
+
+const mealImage = document.querySelector('.meal-option-img');
+const title = document.querySelector('.item-title');
+const price = document.querySelector('.price');
+const desc = document.querySelector('.desc');
+
+const menuGrid = document.querySelector('.menu-grid');
+
+window.addEventListener('DOMContentLoaded', function(){
+    menuGrid.innerHTML = `<div class="meal-option">
+    <img class="meal-option-img" src="https://www.dinneratthezoo.com/wp-content/uploads/2019/12/mozzarella-sticks-4-500x500.jpg">
+    <div class="info">
+        <div class="title-price">
+            <h4 class="item-title">Mozzerella Sticks</h4>
+            <p class="price">$16.99</p>
+        </div>
+
+        <p class="desc">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.</p>
+    </div>
+</div>
+
+<div class="meal-option">
+    <img src="https://images.themodernproper.com/billowy-turkey/production/posts/2019/garlic-bread-10.jpg?w=1200&h=1200&q=82&fm=jpg&fit=crop&fp-x=0.5&fp-y=0.5&dm=1599767628&s=08fa1a2e6dd3daeba5175c6eb9a2d06f"><!--IMG-->
+    <div class="info">
+        <div class="title-price">
+            <h4>Garlic Bread</h4>
+            <p>$16.99</p>
+        </div>
+
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.</p>
+    </div>
+</div>
+
+<div class="meal-option">
+    <img src="https://dinnerthendessert.com/wp-content/uploads/2018/08/Buffalo-Wings-4-500x500.jpg"><!--IMG-->
+    <div class="info">
+        <div class="title-price">
+            <h4>Buffalo Wings</h4>
+            <p>$16.99</p>
+        </div>
+
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.</p>
+    </div>
+</div>
+
+<div class="meal-option">
+    <img src="https://easyweeknightrecipes.com/wp-content/uploads/2020/07/Bruschetta-9-500x500.jpg"><!--IMG-->
+    <div class="info">
+        <div class="title-price">
+            <h4>Fresh Bruschetta</h4>
+            <p>$16.99</p>
+        </div>
+
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.</p>
+    </div>
+</div>
+
+<div class="meal-option">
+    <img src="https://www.spendwithpennies.com/wp-content/uploads/2019/06/Garlic-Grilled-Shrimp-SpendWithPennies-3-500x500.jpg"><!--IMG-->
+    <div class="info">
+        <div class="title-price">
+            <h4>Grilled Shrimp</h4>
+            <p>$16.99</p>
+        </div>
+
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.</p>
+    </div>
+</div>
+
+<div class="meal-option">
+    <img src="https://paleogrubs.com/wp-content/uploads/2014/12/parsnipfriesrecipe.jpg"><!--IMG-->
+    <div class="info">
+        <div class="title-price">
+            <h4>Truffle Fries</h4>
+            <p>$16.99</p>
+        </div>
+
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.</p>
+    </div>
+</div>
+
+<div class="meal-option">
+    <img src="https://www.dinneratthezoo.com/wp-content/uploads/2019/11/fried-calamari-67-500x500.jpg"><!--IMG-->
+    <div class="info">
+        <div class="title-price">
+            <h4>Fried Calamari</h4>
+            <p>$16.99</p>
+        </div>
+
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.</p>
+    </div>
+</div>
+
+<div class="meal-option">
+    <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/stuffed-mushrooms-vertical-jpg-1525207616.jpg?crop=1.00xw:0.667xh;0,0.215xh&resize=480:*"><!--IMG-->
+    <div class="info">
+        <div class="title-price">
+            <h4>Stuffed Mushrooms</h4>
+            <p>$16.99</p>
+        </div>
+
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae eligendi culpa illo, accusamus quas atque.</p>
+    </div>
+</div>
+
+
+
+
+</div><!--Grid End 1-->`
+})
+
+
+options.forEach(function (option){
+    option.addEventListener('click', function(e){
+
+        const dataSetId = e.target.dataset.id;
+
+        const filterMenuItems = menu.filter(function(menuItem){
+            if(dataSetId === menuItem.category){
+                return menuItem;
+            }
+        })
+     
+
+        
+
+        console.log(filterMenuItems);
+
+        
+        //Rn all this does is create the object array and its not putting them together to form the actual
+    })
+})
+
+
+
+  
+
+
+function displayMenuItems(){
+    let displayMenu = menu.map(function(menuItem){
+        return `
+        <div class="meal-option">
+        <img class="meal-option-img" src=${menuItem.image}>
+        <div class="info">
+            <div class="title-price">
+                <h4 class="item-title">${menuItem.title}</h4><!--title and info-->
+                <p class="price">${menuItem.price}</p>
+            </div>
+
+            <p class="desc">${menuItem.desc}</p>
+        </div>
+    </div>`;
+    })
+    console.log(displayMenu);
+    displayMenu = displayMenu.join("");
+    menuGrid.innerHTML = displayMenu;
+}
+
+
+
+
+/*const options = document.querySelectorAll('.option');
+const frame = document.querySelector('.frame');
+const slideColor = document.querySelector('.slide-color');
+
+
+
+options.forEach(function (option){
+    option.addEventListener('click', function(e){
+        if(e.target === options[0]){
+            frame.style.transform = 'translateX(0%)';
+            options[1].style.color = 'black';
+            options[2].style.color = 'black';
+            options[3].style.color = 'black';
+        
+            slideColor.style.left = '0';
+            slideColor.style.background = '#640000';
+        }
+
+        else if(e.target === options[1]){
+            frame.style.transform = 'translateX(-100%)';
+
+            options[1].style.color = '#00641F';
+            options[0].style.color = 'black';
+            options[2].style.color = 'black';
+            options[3].style.color = 'black';
+
+            slideColor.style.left = '25%';
+            slideColor.style.background = '#00641F';
+            
+            
+        }
+        else if(e.target === options[2]){
+            frame.style.transform = 'translateX(-200%)';
+
+            options[2].style.color = '#B90000';
+            options[0].style.color = 'black';
+            options[1].style.color = 'black';
+            options[3].style.color = 'black';
+
+            slideColor.style.left = '50%';
+            slideColor.style.background = '#B90000';
+        }
+        else if(e.target === options[3]){
+            frame.style.transform = 'translateX(-300%)';
+
+            options[3].style.color = '#AF2B63';
+            options[0].style.color = 'black';
+            options[1].style.color = 'black';
+            options[2].style.color = 'black';
+        
+            slideColor.style.left = '75%';
+            slideColor.style.background = '#AF2B63';
+        }
+        
+    });
+})*/
+
